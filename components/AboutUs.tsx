d2475f23decdc192a0d506f90b3953a0763ff013
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-05-22 15:46:21 +0530

    updtes

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -10,8 +10,6 @@ export const AboutUs = () => {
                     Our Story
                 </p>
 
-                {/* <h2 className="text-3xl text-white font-bold pt-4"><span className="border-b-3 border-[#DEAE34]">About PMC Barbershop</span> </h2> */}
-
                 {/* This will show on medium (md) and larger screens */}
                 <h2 className="hidden md:block text-4xl text-white font-bold pt-4 group relative  z-10">
                     <span className="text-white relative after:content-[''] after:absolute after:bottom-0 after:left-72 after:w-20 after:h-[3px] after:bg-[#DEAE34] after:transition-all after:duration-300 group-hover:after:left-0 group-hover:after:w-full">
